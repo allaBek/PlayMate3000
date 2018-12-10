bfa7ff5f08edba4659b08c8fccfc4a1ccdc0f55a
allaBek <mohammed.sayah.bekkoucha@gmail.com>
2018-12-10 10:32:00 -0800

    I added a function to transform from linear to 8*8 list, process the images and output 8*8 game board as 1,-1 abd 0. Also a function that reads thresholds from the txt file.

--- classification_by_circles.py
+++ classification_by_circles.py
@@ -4,12 +4,25 @@
 import datetime
 import cv2
 import numpy as np
+import os
 import math
 from scipy.cluster.vq import vq, kmeans, whiten
 from matplotlib import pyplot as plt
 import operations
 from multiprocessing import Pool
+
 #function to calculate the mean of the array
+def read_txt():
+    txt = open("{}\\{}".format(os.getcwd(), "threshold.txt"))
+    _, player1 = (txt.readline()).split(':')
+    _, player2 = (txt.readline()).split(':')
+    txt.close()
+    values1 = player1.split(',')
+    values2 = player2.split(',')
+    for i in range(0, len(values1)):
+        values1[i] = int(values1[i])
+        values2[i] = int(values2[i])
+    return [values1, values2]
 def getWhitePercentage(thresholded_image):
     h, w = thresholded_image.shape
     s = h * w
@@ -25,25 +38,40 @@ def getWhitePercentage(thresholded_image):
 #Player1:0,64,0,165,49,184
 #Player2:0,180,97,255,68,225
 def classifier(frame_hsv, threshold_values):
+    [values1, values2] = read_txt()
+    [low_H1, high_H1, low_S1, high_S1, low_V1, high_V1] = values1
+    [low_H2, high_H2, low_S2, high_S2, low_V2, high_V2] = values2
+    print(values1)
+    print(values2)
     #apply first threshold range from txt file threshold.txt
-    f1 = cv2.inRange(frame_hsv, (0, 0, 49), (64, 165, 184))
+    f1 = cv2.inRange(frame_hsv, (low_H1, low_S1, low_V1), (high_H1, high_S1, high_V1))
     check1 = getWhitePercentage(f1)
     if check1:
         return 1
     elif not check1:
-        f2 = cv2.inRange(frame_hsv, (0, 97, 68), (180, 255, 225))
+        f2 = cv2.inRange(frame_hsv, (low_H2, low_S2, low_V2), (high_H2, high_S2, high_V2))
         check2 = getWhitePercentage(f2)
         if check2:
             return -1
         else:
             return 0
-def pieces_matrix(matrix):
+def pieces_matrix(list):
+    matrix = line_to_square(list)
     output = np.zeros((8, 8))
     for i in range(0,8):
         for j in range(0,8):
             frame_HSV = cv2.cvtColor(matrix[i][j] , cv2.COLOR_BGR2HSV)
             output[i,j] = classifier(frame_HSV, 1)
     return output
+def line_to_square(list):
+    matrix = []
+    row = []
+    for i in range(0,64):
+        row.append(list[i])
+        if (i+1)%8 == 0:
+            matrix.append(row)
+            row = []
+    return matrix
 def simpleCheck():
     correct_values = 0
     for i in range(1, 315):
@@ -66,6 +94,8 @@ def simpleCheck():
             print(str(i) + "     " + str(guess) + "     False")
     print('Accuracy:    '+ str(correct_values) +"/314")
 if __name__ == '__main__':
-    row = []
-    matrix = []
-    simpleCheck()
+    list = []
+    for i in range(1,65):
+        img = cv2.imread(r"C:\Users\moham\Desktop\myData\pics\({}).jpg".format(i))
+        list.append(img)
+    print(pieces_matrix(list))
